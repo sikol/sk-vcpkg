4a462e3dd190ded19586e719a3c3bf6b03550bee
Liss Tarnell <felicity@sikol.co.uk>
2021-03-10 17:07:05 +0000

    update sk-error

--- ports/sk-error/portfile.cmake
+++ ports/sk-error/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO sikol/sk-error
   REF c6ca1689e4c3427ae41e54de2feb31a6c7c9235f
-  SHA512 1
+  SHA512 dafea2f9e3e2fbaed1c35f767dd3ce0ae5e637a817e5830c292ccd0dec82918e23e2565a967e8279ed90e584c1e3868756f17a2bfd777f3bd5d2592789b6694b
   HEAD_REF master
 )
 
